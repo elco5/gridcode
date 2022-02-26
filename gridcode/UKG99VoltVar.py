5e7cc2a691151b7bbf4d1a1f4a94bb1ccee8531c
ec57 <countrywoodster@gmail.com>
2022-02-26 09:28:24 -0800

    from my laptop

---
+++ gridcode/UKG99VoltVar.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+class UKG99VoltVar:
+    
+
+    def __init__(self, characteristic_type, power_level, eut) -> None:
+        
+        self.characteristic = characteristic_type
+        self.eut = eut
+
+        if characteristic_type == 1:
+            self.Q = np.array([1, 0, 0, -1]) *eut.qmax * power_level
+            self.V = np.array([0.95, 1, 1, 1.05]) * eut.vn
+
+        elif characteristic_type == 2:
+            self.Q = np.array([0.5, 0, 0, -0.5]) *eut.qmax * power_level
+            self.V = np.array([0.9, 0.98, 1.02, 1.1]) * eut.vn
+
+        elif characteristic_type == 3:
+            self.Q = np.array([0.05, 0, 0, -0.05]) *eut.qmax * power_level
+            self.V = np.array([0.82, 0.95, 1.05, 1.18]) * eut.vn
+
+    def vars_from_volts(self, volts):
+
+        if volts <= self.V[1]:
+            dy = self.Q[1] - self.Q[0]
+            dx = self.V[1] - self.V[0]
+            return (dy/dx) * (volts - self.V[1])
+        elif (volts > self.V[1] and volts < self.V[2]):        
+            return 0
+        elif volts >= self.V[2]:
+            dy = self.Q[3] - self.Q[2]
+            dx = self.V[3] - self.V[2]
+            return (dy/dx) * (self.V[3] - volts)
