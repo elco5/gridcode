a615b0ef57eb9d17e3432ff2dcfd5038623712cb
ec57 <countrywoodster@gmail.com>
2022-03-03 09:06:23 -0800

    keeping up to date

--- gridcode/py_driver.py
+++ gridcode/py_driver.py
@@ -3,7 +3,7 @@ import powerwall, voltwatt, reportdriver, bento
 
 report = reportdriver.ReportDriver()    # new report
 pw = powerwall.Powerwall()          # create eut
-gtc = bento.Bento()
+bto = bento.Bento()
 vw1 = voltwatt.VoltWattTest(1, pw)  # create test data profile
 
 ideal = report.calc_ideal_power_data_frame(vw1)
